02894a2469c650d96cc753f5d68a9ef856dd2e07
TAlh-a-Sajid <raot42556@gmail.com>
2026-05-13 23:12:06 +0500

    finally pushing the damn code

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string name = "Talha Sajid";
+int age = 19 ;
+string city = "vehari";
+int heightInMeters = 178;
+string address = "39WBvehari";
+string uni = "UET";
+string school = "TheEducators";
+string college = "PunjabCollege";
+int weight = 74;
+
+int main (){
+    cout << " these are dtails of Talha Sajid" << endl;
+    cout << "there was no other way this had to be done " << endl;
+    cout << " if not me then who could" ;
+    cout << "a purpose given to me "<< endl;
+    cout << "only i could do it" << endl;
+    cout << " what do you think of that L ?" << endl;
+    cout << " this is my perfect victory" << endl; 
+
+cout << name << endl;
+cout << age << endl;
+cout << city << endl;
+cout << heightInMeters << endl;
+cout << address << endl;
+cout << uni << endl; 
+cout << school << endl;
+cout << college << endl;
+cout << weight << endl;
+
+cout << " the end " << endl;
+return 0;
+
+
+
+
+
+}
