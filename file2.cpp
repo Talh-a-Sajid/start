6f79695dd33650f190ca554ffc37c967a240cad6
TAlh-a-Sajid <raot42556@gmail.com>
2026-05-15 22:17:31 +0500

    cahnges made

--- file2.cpp
+++ file2.cpp
@@ -6,3 +6,15 @@ chal uth on pagla
 dil ronda ha jigra
 inu chup karaday
 inu edi heer miladay
+
+
+
+
+making changes 
+
+
+kadi ta has bol wa aa a a  a a a a a  
+na dil sada rol way
+tu aja dil janiya aaaaaa
+tu aja dil janaia aaaaaa
+na dukha naal rol way ayayvaayayyayaya
